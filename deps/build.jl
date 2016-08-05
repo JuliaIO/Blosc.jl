45fee19f2ba6f407765207e3a08f184f7ec066ba
ranjanan <benditlikeranjan@gmail.com>
2016-08-05 16:23:34 +0530

    use Compat.KERNEL instead of OSNAME

--- deps/build.jl
+++ deps/build.jl
@@ -2,15 +2,14 @@ using BinDeps
 using Compat
 
 vers = "1.8.1"
-const OSNAME = is_windows() ? :Windows : Compat.KERNEL
 
 tagfile = "installed_vers"
 target = "libblosc.$(Libdl.dlext)"
 url = "https://bintray.com/artifact/download/julialang/generic/"
 if !isfile(target) || !isfile(tagfile) || readchomp(tagfile) != "$vers $(Sys.WORD_SIZE)"
-    if OSNAME == :Windows
+    if Compat.KERNEL == :NT
         run(download_cmd(url*"libblosc$(Sys.WORD_SIZE)-$vers.dll", target))
-    elseif OSNAME == :Darwin
+    elseif Compat.KERNEL == :Darwin
         run(download_cmd(url*"libblosc$(Sys.WORD_SIZE)-$vers.dylib", target))
     else
         tarball = "c-blosc-$vers.tar.gz"
