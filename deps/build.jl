2d906ba8e00b688602b9cbfbc9fdba4fd8be1f2f
Steven G. Johnson <stevenj@mit.edu>
2018-04-02 17:06:32 -0400

    Sys.WORD_SIZE is not in Compat

--- deps/build.jl
+++ deps/build.jl
@@ -1,16 +1,15 @@
 using Compat
-import Compat.Sys, Compat.Libdl
 
 vers = "1.12.1"
 
 tagfile = "installed_vers"
-target = "libblosc.$(Libdl.dlext)"
+target = "libblosc.$(Compat.Libdl.dlext)"
 url = "https://bintray.com/artifact/download/julialang/generic/"
 
 if !isfile(target) || !isfile(tagfile) || readchomp(tagfile) != "$vers $(Sys.WORD_SIZE)"
-    if Sys.iswindows()
+    if Compat.Sys.iswindows()
         download(url*"libblosc$(Sys.WORD_SIZE)-$vers.dll", target)
-    elseif Sys.isapple()
+    elseif Compat.Sys.isapple()
         download(url*"libblosc$(Sys.WORD_SIZE)-$vers.dylib", target)
     else
         tarball = "c-blosc-$vers.tar.gz"
