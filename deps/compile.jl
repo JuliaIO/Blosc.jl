0a4cb80b5693a34ed72aee3e59bda78af3c37c0f
Steven G. Johnson <stevenj@mit.edu>
2018-05-17 12:39:05 -0400

    whoops, cp symlinks

--- deps/compile.jl
+++ deps/compile.jl
@@ -23,7 +23,7 @@ function compile(libname, tarball_url, hash; prefix=BinaryProvider.global_prefix
         run(`$cmake_executable -DBUILD_TESTS=Off -DBUILD_BENCHMARKS=Off ..`)
         run(`$cmake_executable --build .`)
         mkpath(libdir(prefix))
-        mv("blosc/libblosc.$dlext", joinpath(libdir(prefix), libname*"."*dlext),
-           remove_destination=true)
+        cp("blosc/libblosc.$dlext", joinpath(libdir(prefix), libname*"."*dlext),
+           remove_destination=true, follow_symlinks=true)
     end
 end
